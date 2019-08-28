e1f8157ab17137bc33de793519a2b4f8bcfbf5e3
simonlow <simonlow@152.23.240.156>
2019-08-28 14:35:53 -0400

    c

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,45 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		System.out.println("Enter total number of customers.");
+		int c = scan.nextInt();
+		
+		String[] fnames = new String[c];
+		String[] lnames = new String[c];
+		double [] prices = new double[c]; 
+		
+		
+		
+		for (int i = 0; i< c; i++)
+		{
+			System.out.println("Enter first name, last name,"
+					+ " and how many types of items");
+			fnames[i] = scan.next();
+			lnames[i] = scan.next();
+			int num = scan.nextInt();
+			
+			for (int j = 0; j< num; j++)
+			{
+				System.out.println("put number of item, name of item, and price");
+				int n = scan.nextInt();
+				
+				String item = scan.next();
+				
+				double p = scan.nextDouble();
+				
+				prices[i]+= (p*(double)n);
+						
+			}
+				
+		}
+		
+		for (int i = 0; i < c; i++)
+		{
+			System.out.println( fnames[i].charAt(0) + ". " + lnames[i] 
+			+ " " + prices[i]);
+		}
+		
+		
 		
 	}
 }
