80bb2c0c3965f25427b47d351f9c6a0ad13a49b1
simonlow <simonlow@152.23.240.156>
2019-08-29 16:10:48 -0400

    new

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -13,7 +13,8 @@ public class A1Novice {
 		
 		String[] fnames = new String[c];
 		String[] lnames = new String[c];
-		double [] prices = new double[c]; 
+		String[] prices = new String[c]; 
+		double[] ps = new double[c];
 		
 		
 		
@@ -25,17 +26,38 @@ public class A1Novice {
 			int num = scan.nextInt();
 			
 			for (int j = 0; j< num; j++)
-			{
+			{ 
 				//enter number of that item, name, and price
 				int n = scan.nextInt();
 				
 				String item = scan.next();
 				
 				double p = scan.nextDouble();
+				ps[i]+= p*(double)n;
 				
-				prices[i]+= (p*(double)n);
+				
+				
 						
 			}
+			int phun = (int)(ps[i]*100);
+			
+			
+			String pri = Integer.toString(phun);
+			String pric;
+			if (pri.length() < 3) {
+				pric = "." + pri;
+			}
+			else if (pri.length() == 3 ) 
+			{
+				pric = pri.charAt(0) + "." + pri.substring(1,3);
+			}
+			else 
+			{
+				pric = pri.substring(0, pri.length() - 2) + "." 
+			+ pri.substring(pri.length() - 2, pri.length());
+			}
+			
+			prices[i] = pric;
 				
 		}
 		
